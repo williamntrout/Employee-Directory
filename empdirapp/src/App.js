fa62d577f51962e0f279f2968f968b7fd2d4a46c
William Trout <troutw5593@gmail.com>
2020-07-23 22:28:16 -0500

    working on js

--- empdirapp/src/App.js
+++ empdirapp/src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
-import Container from "./components/Container";
+// import Wrapper from "./components/Wrapper";
+// import Header from "./components/Header";
+import SearchBox from "./components/SearchBox";
+import Table from "./components/Table";
+// import Footer from "./components/Footer";
 
 function App() {
-  return <Container />;
+  return (
+    <div>
+      <SearchBox />
+      <Table />
+    </div>
+  );
 }
 
 export default App;
