8380b1c8caaa072a5715b6875761983b47e4fc01
William Trout <troutw5593@gmail.com>
2020-07-30 21:22:08 -0500

    Updates

---
+++ precache-manifest.4cbb78dfe48bf8ad283ae01d362cf4e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a8fcf64ebf6b90ec75688396f9ef79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d51e6e9fb9510220a0c3",
+    "url": "/static/css/main.d58cd4fa.chunk.css"
+  },
+  {
+    "revision": "2191e294e51467e0bf1b",
+    "url": "/static/js/2.1ec2bb0e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1ec2bb0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d51e6e9fb9510220a0c3",
+    "url": "/static/js/main.a0284c92.chunk.js"
+  },
+  {
+    "revision": "8e6dd9f48e63b5725b60",
+    "url": "/static/js/runtime-main.3ac080aa.js"
+  }
+]);
